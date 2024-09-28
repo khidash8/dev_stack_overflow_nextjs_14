910176965815ec0a1ed22a8e0186f38818b64eae
DevApe <khidash8@gmail.com>
2024-09-28 08:45:42 +0530

    shad cn react hook form and tiny editor integration

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,15 @@
+import { Question } from "@/components/forms/question";
+
 const page = () => {
-  return <div>Ask Questions</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a questions</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId="bluhh" />
+      </div>
+    </div>
+  );
 };
 
 export default page;
